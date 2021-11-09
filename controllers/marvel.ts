b721d9d36dedb64a092b964f63eaeb7992884114
Fadlika Dita Nurjanto <fadlikadn@gmail.com>
2021-11-09 22:14:30 +0700

    Change get character endpoint to call subsequent api (still without cache)

--- controllers/marvel.ts
+++ controllers/marvel.ts
@@ -13,16 +13,45 @@ export default class MarvelController {
     @Get("/characters")
     public async getCharacters(): Promise<CharacterIds> {
         const hash = md5(`${marvelConfig.ts}${marvelConfig.privateKey}${marvelConfig.publicKey}`);
-        const url = `${marvelConfig.url}characters?apikey=${marvelConfig.publicKey}&hash=${hash}&ts=${marvelConfig.ts}`;
-        try {
-            const { data } = await axios.get(url);
-            const ids = extractCharacterIds(data as CharacterDataWrapper);
-            return ids;
-        } catch (err) {
-            console.log(err);
-            const errResult: CharacterIds = [];
-            return errResult;
+        let storedIds: CharacterIds = [];
+        const limit = 100;
+        let offset = 0;
+        let start: boolean = true;
+        let url = "";
+
+        while (start)  {
+            url = `${marvelConfig.url}characters?apikey=${marvelConfig.publicKey}&hash=${hash}&ts=${marvelConfig.ts}&limit=${limit}&offset=${offset}`;
+            try {
+                console.log(url);
+                console.log(offset, limit);
+                const { data } = await axios.get(url);
+                const ids = extractCharacterIds(data as CharacterDataWrapper);
+                console.log(ids.length);
+                storedIds = [
+                    ...storedIds,
+                    ...ids,
+                ];
+                offset += ids.length;
+                if (ids.length < 100) {
+                    start = false;
+                }
+            } catch (err) {
+                console.log(err);
+                start = false;
+            }
         }
+
+        return storedIds;
+
+        // try {
+        //     const { data } = await axios.get(url);
+        //     const ids = extractCharacterIds(data as CharacterDataWrapper);
+        //     return ids;
+        // } catch (err) {
+        //     console.log(err);
+        //     const errResult: CharacterIds = [];
+        //     return errResult;
+        // }
     }
 
     @Get("/characters/{id}")
